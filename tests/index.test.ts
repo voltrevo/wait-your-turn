fe6184e07bdb4e5b1b871ab9b25cf51fdf867e41
Andrew Morris <voltrevo@gmail.com>
2022-12-20 13:24:09 +1100

    Add eslint, autofixes

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,136 +1,144 @@
-import { assert } from 'chai';
-import { Semaphore, Mutex } from '../src';
+import {assert} from 'chai';
+import {Semaphore, Mutex} from '../src';
 
-export function delay(ms: number) {
-    return new Promise<void>((res, rej) => setTimeout(res, ms));
+export async function delay(ms: number) {
+	return new Promise<void>((res, rej) => setTimeout(res, ms));
 }
 
-describe('util', function() {
-    describe('semaphore', function() {
-        it('limits concurrency', async function() {
-            var s = new Semaphore(2);
-            var running = 0;
-            var ran = 0;
-            var task = async () => {
-                var release = await s.acquire();
-                assert(running <= 1);
-                running++;
-                await delay(10);
-                assert(running <= 2);
-                running--;
-                ran++;
-                release();
-            };
-            await Promise.all([1,2,3,4,5].map(i => task()));
-            assert.equal(ran, 5);
-        });
+describe('util', () => {
+	describe('semaphore', () => {
+		it('limits concurrency', async () => {
+			const s = new Semaphore(2);
+			let running = 0;
+			let ran = 0;
+			const task = async () => {
+				const release = await s.acquire();
+				assert(running <= 1);
+				running++;
+				await delay(10);
+				assert(running <= 2);
+				running--;
+				ran++;
+				release();
+			};
 
-        it('limits concurrency (use syntax)', async function() {
-            var s = new Semaphore(2);
-            var running = 0;
-            var ran = 0;
-            var task = async () => {
-                assert(running <= 1);
-                running++;
-                await delay(10);
-                assert(running <= 2);
-                running--;
-                ran++;
-            };
-            await Promise.all([1,2,3,4,5].map(i => s.use(task)));
-            assert.equal(ran, 5);
-        });
+			await Promise.all([1, 2, 3, 4, 5].map(async i => task()));
+			assert.equal(ran, 5);
+		});
 
-        it('use recovers from thrown exception', async function() {
-            var s = new Semaphore(2);
-            var running = 0;
-            var ran = 0;
-            var erred = 0;
-            var task = (i) => async () => {
-                assert(running <= 1);
-                running++;
-                await delay(10);
-                assert(running <= 2);
-                running--;
-                if (i === 2) {
-                    throw new Error('bogus');
-                }
-                ran++;
-            };
-            await s.use(task(1));
-            try {
-                await s.use(task(2));
-            } catch (err) {
-                erred++;
-            }
-            await s.use(task(3));
-            await s.use(task(4));
-            await s.use(task(5));
-            assert.equal(ran, 4);
-            assert.equal(erred, 1);
-            assert.equal(s.count, 2);
-        });
+		it('limits concurrency (use syntax)', async () => {
+			const s = new Semaphore(2);
+			let running = 0;
+			let ran = 0;
+			const task = async () => {
+				assert(running <= 1);
+				running++;
+				await delay(10);
+				assert(running <= 2);
+				running--;
+				ran++;
+			};
 
-    });
+			await Promise.all([1, 2, 3, 4, 5].map(async i => s.use(task)));
+			assert.equal(ran, 5);
+		});
 
-    describe('mutex', function() {
-        it('tasks do not overlap', function(done) {
-            var m = new Mutex();
-            var task1running = false;
-            var task2running = false;
-            var task1ran = false;
-            var task2ran = false;
-            Promise.all([
-                m.acquire()
-                .then(release => {
-                    task1running = true;
-                    task1ran = true;
-                    return delay(10)
-                    .then(() => {
-                        assert(!task2running);
-                        task1running = false;
-                        release();
-                    });
-                }),
-                m.acquire().
-                then(release => {
-                    assert(!task1running);
-                    task2running = true;
-                    task2ran = true;
-                    return delay(10)
-                    .then(() => {
-                        task2running = false;
-                        release();
-                    });
-                })
-            ])
-            .then(() => {
-                assert(!task1running);
-                assert(!task2running);
-                assert(task1ran);
-                assert(task2ran);
-                done();
-            })
-            .catch(done);
-        });
-        it('double lock deadlocks', function(done) {
-            var m = new Mutex();
-            m.acquire()
-            .then(r => m.acquire())
-            .then(r => assert(false))
-            .catch(done);
-            delay(10)
-            .then(done);
-        });
-        it('double release ok', function(done) {
-            var release;
-            var m = new Mutex();
-            m.acquire().
-                then(r => release = r).
-                then(() => release()).
-                then(() => release());
-            m.acquire().
-                then(r => done());
-        });
-    });
+		it('use recovers from thrown exception', async () => {
+			const s = new Semaphore(2);
+			let running = 0;
+			let ran = 0;
+			let erred = 0;
+			const task = i => async () => {
+				assert(running <= 1);
+				running++;
+				await delay(10);
+				assert(running <= 2);
+				running--;
+				if (i === 2) {
+					throw new Error('bogus');
+				}
+
+				ran++;
+			};
+
+			await s.use(task(1));
+			try {
+				await s.use(task(2));
+			} catch (err) {
+				erred++;
+			}
+
+			await s.use(task(3));
+			await s.use(task(4));
+			await s.use(task(5));
+			assert.equal(ran, 4);
+			assert.equal(erred, 1);
+			assert.equal(s.count, 2);
+		});
+	});
+
+	describe('mutex', () => {
+		it('tasks do not overlap', done => {
+			const m = new Mutex();
+			let task1running = false;
+			let task2running = false;
+			let task1ran = false;
+			let task2ran = false;
+			Promise.all([
+				m.acquire()
+					.then(async release => {
+						task1running = true;
+						task1ran = true;
+						return delay(10)
+							.then(() => {
+								assert(!task2running);
+								task1running = false;
+								release();
+							});
+					}),
+				m.acquire()
+					.then(async release => {
+						assert(!task1running);
+						task2running = true;
+						task2ran = true;
+						return delay(10)
+							.then(() => {
+								task2running = false;
+								release();
+							});
+					}),
+			])
+				.then(() => {
+					assert(!task1running);
+					assert(!task2running);
+					assert(task1ran);
+					assert(task2ran);
+					done();
+				})
+				.catch(done);
+		});
+		it('double lock deadlocks', done => {
+			const m = new Mutex();
+			m.acquire()
+				.then(async r => m.acquire())
+				.then(r => {
+					assert(false);
+				})
+				.catch(done);
+			delay(10)
+				.then(done);
+		});
+		it('double release ok', done => {
+			let release;
+			const m = new Mutex();
+			m.acquire()
+				.then(r => release = r)
+				.then(() => release())
+				.then(() => release());
+			m.acquire()
+				.then(r => {
+					done();
+				});
+		});
+	});
 });
